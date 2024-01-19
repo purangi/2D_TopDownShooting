b25232cf374f4b51383ae0b63c4d83ad9f01797a
purangi <kaya0728@naver.com>
2024-01-19 11:15:08 +0900

    Create: 2D_TopDownShooting 업로드
    1) 플레이어 생성 및 컨트롤러 생성
    2) 던전 타일맵 제작

---
+++ Assets/Scripts/Controllers/TopDownCharacterController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController : MonoBehaviour
+{
+    //event 외부에서는 호출하지 못하게 막는다.
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+}
+
+//{
+//    //public float speed = 5f;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        //float x = Input.GetAxisRaw("Horizontal");
+//        //float y = Input.GetAxisRaw("Vertical");
+
+//        //transform.position += new Vector3(x, y) * speed * Time.deltaTime;
+//    }
+//}
